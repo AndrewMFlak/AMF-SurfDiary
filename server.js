4265f4ee48ee09d0e72542e1070870feee90f751
AFlak <AndrewMFlak@gmail.com>
2020-03-15 22:19:41 -0400

    update

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ mongoose.connect(
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
     console.log('http://localhost:3000/')
-    console.log('http://localhost:3001/')
     console.log('http://localhost:3001/api/spots')
     console.log('https://blooming-coast-38158.herokuapp.com/') 
     console.log('<br>');
